417997b4c412a418871f188f42836d90b51a9aad
rajchaudhari09 <rajchaudhari366@gmail.com>
2020-03-13 10:20:11 +0530

    REFACTORED: Use Case Programming Concept Adding

--- employee_wage.sh
+++ employee_wage.sh
@@ -3,14 +3,16 @@
 partTime=1
 fullTime=2
 employeeRatePerHours=20
-randomCheck=$((RANDOM%3))
-if [[ $fullTime -eq $randomCheck ]]
-then
-	employeeHours=8
-elif [[ $partTime -eq $randomCheck ]]
-then
-	employeeHours=4
-else
-	employeeHours=0
-fi
+empCheck=$((RANDOM%3))
+	case $empCheck in
+		$fullTime)
+			employeeHours=8
+			;;
+		$partTime)
+			employeeHours=4
+			;;
+		*)
+			employeeHours=0
+			;;
+esac
 salary=$(($employeeHours*$employeeRatePerHours))

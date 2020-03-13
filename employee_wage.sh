cdfdb1f7d12af5616292e52a163e4ae6549c0bf5
rajchaudhari09 <rajchaudhari366@gmail.com>
2020-03-13 12:14:12 +0530

    REFACTORED: Refactor The Code To Write a Function to Get Work Hours

--- employee_wage.sh
+++ employee_wage.sh
@@ -3,7 +3,7 @@
 # CONSTANT FOR THE PROGRAM
 PART_TIME=1
 FULL_TIME=2
-MAXIMUM_HOURS_IN_MONTH=10
+MAXIMUM_HOURS_IN_MONTH=4
 EMPLOYEE_RATE_PER_HOURS=20
 NUM_WORKING_DAYS=20
 
@@ -11,23 +11,28 @@ NUM_WORKING_DAYS=20
 totalEmployeeHours=0
 totalWorkingDays=0
 
+#FUNCTION
+function getWorkingHours()
+{
+	case $1 in
+		$FULL_TIME)
+			workHours=8
+			;;
+		$PART_TIME)
+			workHours=4
+			;;
+		*)
+			workHours=0
+			;;
+	esac
+	echo $workHours
+}
 
-while [[ $totalEmployeeHours -lt $MAXIMUM_HOURS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+while [[ $totalWorkHours -lt $MAXIMUM_HOURS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
 	((totalWorkingDays++))
-	empCheck=$((RANDOM%3))
-		case $empCheck in
-			$FULL_TIME)
-				employeeHours=8
-				;;
-			$PART_TIME)
-				employeeHours=4
-				;;
-			*)
-				employeeHours=0
-				;;
-		esac
-		totalEmployeeHours=$(($totalEmployeeHours*$employeeHours))
+	workHours="$( getWorkingHours $((RANDOM%3)) )"
+	totalWorkHours=$(($totalWorkHours+$workHours))
 done
-totalSalary=$(($totalEmployeeHours+$EMPLOYEE_RATE_PER_HOURS))
+totalSalary=$(($totalWorkHours*$EMPLOYEE_RATE_PER_HOURS))
 
